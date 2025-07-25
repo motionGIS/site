957e825096286cf13d983b9410d61c14a566b8d8
aw <aw@a0.ax>
2025-07-25 14:34:29 -0600

    add shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from "$lib/components/header.svelte";
+	import HeroSection from "$lib/components/hero-section.svelte";
+</script>
+
+<div class="min-h-screen bg-background text-foreground flex flex-col">
+	<Header />
+	<HeroSection />
+</div>
